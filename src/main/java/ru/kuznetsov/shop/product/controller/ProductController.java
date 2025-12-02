458f2a3ecb76fc4e95b784f3c76df79b6adbfc8c
Pavel Kuznetsov <Technic47@gmail.com>
2025-12-02 15:47:49 +0300

    Method update.

--- src/main/java/ru/kuznetsov/shop/product/controller/ProductController.java
+++ src/main/java/ru/kuznetsov/shop/product/controller/ProductController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.kuznetsov.shop.data.service.KafkaService;
@@ -26,6 +28,8 @@ import static ru.kuznetsov.shop.represent.common.KafkaConst.PRODUCT_SAVE_TOPIC;
 @RequestMapping("/product")
 @RequiredArgsConstructor
 public class ProductController {
+
+    private static final Integer DEFAULT_PAGE_SIZE = 10;
 
     private final ProductService productService;
     private final ProductPagingAndSortingService pagingAndSortingService;
@@ -61,10 +65,14 @@ public class ProductController {
 
     @GetMapping("/card/page")
     public ResponseEntity<Page<ProductCardDto>> getAllCardPageable(
-            @RequestParam Pageable pageable,
+            @RequestParam Integer pageNumber,
+            @RequestParam(required = false) Integer pageSize,
+            @RequestParam(required = false) String order,
+            @RequestParam(required = false) String[] sortBy,
             @RequestParam(required = false) Long categoryId,
             @RequestParam(required = false) String ownerId
     ) {
+        Pageable pageable = getPageable(pageNumber, pageSize, order, sortBy);
         if (categoryId != null || ownerId != null) {
             return ResponseEntity.ok(pagingAndSortingService.findAllByCategoryOrOwnerIdPageable(
                     ownerId == null ? null : UUID.fromString(ownerId),
@@ -109,5 +117,24 @@ public class ProductController {
         if (!sendResult) {
             logger.warn("Failed to send product to topic. Product: {} operation id {}", productDto, uuidString);
         }
+    }
+
+    private Pageable getPageable(
+            Integer pageNumber,
+            Integer pageSize,
+            String order,
+            String[] sortBy
+    ) {
+        pageSize = pageSize == null ? DEFAULT_PAGE_SIZE : pageSize;
+
+        if (sortBy != null && sortBy.length > 0) {
+            return PageRequest.of(
+                    pageNumber,
+                    pageSize,
+                    Sort.by(Sort.Direction.fromString(
+                            order != null && order.equalsIgnoreCase("ASC") ? "ASC" : "DESC"
+                    ), sortBy)
+            );
+        } else return PageRequest.of(pageNumber, pageSize);
     }
 }
